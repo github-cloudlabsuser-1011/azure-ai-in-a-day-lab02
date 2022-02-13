ca6d16ba9a871e77537548c7fa9283025c7ab69a
github-cloudlabsuser-1011 <81294013+github-cloudlabsuser-1011@users.noreply.github.com>
2022-02-13 22:04:31 +0200

    Update COVID19Articles_build_train_pipeline.py

--- COVID19Articles/ml_service/pipelines/COVID19Articles_build_train_pipeline.py
+++ COVID19Articles/ml_service/pipelines/COVID19Articles_build_train_pipeline.py
@@ -101,7 +101,7 @@ def main():
 
     # Create training output data to pass to evaluate step
     train_output_data = OutputFileDatasetConfig(
-        name="train_output", 
+        name="train_output",
         destination=(aml_workspace.get_default_datastore(), "training/{run-id}/model_metrics.json")).as_upload()
 
     train_step = PythonScriptStep(
